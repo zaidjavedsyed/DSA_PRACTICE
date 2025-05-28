429260cab5e20d2c3b7da7580f97b1dfba485a8e
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-05-28 11:48:12 +0530

    Time: 12 ms (71.18%), Space: 44.3 MB (96.11%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,25 +1,13 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int n = s.length();
-        // last seen positions, initialize to -1
-        int lastA = -1, lastB = -1, lastC = -1;
-        int ans = 0;
-        
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            if (ch == 'a') lastA = i;
-            else if (ch == 'b') lastB = i;
-            else /* ch == 'c' */ lastC = i;
-            
-            // Earliest index among lastA, lastB, lastC:
-            int minLast = Math.min(lastA, Math.min(lastB, lastC));
-            if (minLast >= 0) {
-                // Any substring ending at i that starts at [0..minLast]
-                // will include all three at least once
-                ans += (minLast + 1);
-            }
+     int[] lastscene = {-1,-1,-1};
+     int count =0;
+     for(int i =0;i<s.length();i++){
+        lastscene[s.charAt(i)-'a']=i;
+        if(lastscene[0]!=-1 && lastscene[1]!=-1 && lastscene[2]!=-1){
+            count = count +1+ Math.min(lastscene[0],Math.min(lastscene[1],lastscene[2]));
         }
-        
-        return ans;
+     }
+     return count;
     }
 }
