a5039236af1d0dd7c2afe66a68443157d9da5600
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-06-01 21:28:20 +0530

    Time: 3 ms (50.55%), Space: 45.7 MB (70.78%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       int furthest =0;
+       for(int i=0;i<nums.length;i++){
+            if(i>furthest){
+                return false;
+            }
+            furthest=Math.max(furthest,i+nums[i]);
+       }
+       return true;
+    }
+}
