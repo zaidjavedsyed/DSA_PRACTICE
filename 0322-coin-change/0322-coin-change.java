01c0bb83e33abb6b9adcda5c6edbe3d9c6954101
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-08-16 20:01:19 +0530

    Time: 18 ms (41.01%), Space: 45.6 MB (14.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int find(int ind,int target,int[] arr,int[][] dp){
+  /*  public int find(int ind,int target,int[] arr,int[][] dp){
         if(target==0){
             return 0;
         }
@@ -28,12 +28,12 @@ class Solution {
         dp[ind][target] = Math.min(take,ntake);
         return Math.min(take,ntake);
         }
-    
+    */
 
     public int coinChange(int[] coins, int amount) {
     
     int[][] dp= new int[coins.length][amount+1];
-    for(int i=0;i<coins.length;i++){
+    /*for(int i=0;i<coins.length;i++){
         for(int j=0;j<=amount;j++){
             dp[i][j] = -5;
         }
@@ -44,5 +44,37 @@ class Solution {
     }else{
         return ans;
     }
-
+    */
+    //tabulation
+    for(int i=0;i<coins.length;i++){
+    dp[i][0] = 0;
+    }
+    for(int t=1;t<=amount;t++){
+        if(coins[0]<=t){
+            if(t%coins[0]==0){
+            dp[0][t] =t/coins[0];
+            }else{
+                dp[0][t] = Integer.MAX_VALUE;
+            }
+    }else{
+        dp[0][t] = Integer.MAX_VALUE;
+    }}
+    for(int ind=1 ;ind<coins.length;ind++){
+        for(int target = 1;target<=amount;target++){
+            int take = Integer.MAX_VALUE;
+            if(coins[ind]<=target){
+                take = dp[ind][target-coins[ind]];
+         }
+            if(take!=Integer.MAX_VALUE){
+                take+=1;
+            }
+            int ntake = dp[ind-1][target];
+            dp[ind][target] = Math.min(take,ntake);
+            }
+        }
+    if(dp[coins.length-1][amount]==Integer.MAX_VALUE){
+        return -1;
+    }else{
+        return dp[coins.length-1][amount];
+    }
     }}
