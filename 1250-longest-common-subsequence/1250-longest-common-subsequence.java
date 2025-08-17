49bffbfd0abebc727a350aec53e26f1c9133fba9
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-08-17 18:15:36 +0530

    Time: 22 ms (41.88%), Space: 45.2 MB (91.91%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -21,7 +21,7 @@ class Solution {
     }
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+       // int[][] dp = new int[text1.length()+1][text2.length()+1];
         /*        
         for(int i=0;i<=text1.length();i++){
             for(int j=0;j<=text2.length();j++){
@@ -31,6 +31,7 @@ class Solution {
         return find(text1.length(),text2.length(),text1,text2,dp);
         */
         //tabulation
+        /*
         for(int j=0;j<=text2.length();j++){
             dp[0][j] = 0;
         }
@@ -50,7 +51,32 @@ class Solution {
                     dp[i][j] = max1;
                  } }
         }
-        return dp[text1.length()][text2.length()];
+        return dp[text1.length()][text2.length()];*/
+        //2X 1d array approach
+        int[] prev = new int[text2.length()+1];
+        int[] curr = new int[text2.length()+1];
+        for(int j=0;j<=text2.length();j++){
+            prev[j] = 0;
+        }
+     
+        for(int i=0;i<=text1.length();i++){
+            curr[0]=0;
+        }
+        for(int i=1;i<=text1.length();i++){
+            for(int j=1;j<=text2.length();j++){
+                int take = 0;
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    curr[j]=prev[j-1]+1;
+                }
+                else{
+                    int iback=prev[j];
+                    int jback = curr[j-1];
+                    int max1= Math.max(iback,jback);
+                    curr[j] = max1;
+                 } }
+                 prev = curr.clone();
+        }
+        return prev[text2.length()];
 
 
     }
