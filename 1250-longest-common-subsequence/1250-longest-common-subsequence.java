a71ab3b0de48cf6c8fb8bd886f9eed2db13ddcb0
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-08-17 18:02:05 +0530

    Time: 29 ms (15.3%), Space: 50.9 MB (67.83%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,87 +1,36 @@
 class Solution {
+    // We are going to solve this by shifting of index so if i=n it means n-1or i==0 then i =-1 this is to solve base case problem for tabulation 
     public int find(int i,int j,String text1,String text2,int[][] dp){
-        if(i==0){
-            for(int t=j;t>=0;t--){
-                if(text1.charAt(i)==text2.charAt(t)){
-                    return 1;
-                }
-            }
-            return 0;
-        }
-        if(j==0){
-            for(int t=i;t>=0;t--){
-                if(text1.charAt(t)==text2.charAt(j)){
-                    return 1;
-                }
-            }
+        if(i==0 || j==0){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         int take = 0;
-        if(text1.charAt(i)==text2.charAt(j)){
-            take = find(i-1,j-1,text1,text2,dp)+1;
+        if(text1.charAt(i-1)==text2.charAt(j-1)){
+            dp[i][j]=find(i-1,j-1,text1,text2,dp)+1;
+            return find(i-1,j-1,text1,text2,dp)+1;
         }
         int iback=find(i-1,j,text1,text2,dp);
         int jback = find(i,j-1,text1,text2,dp);
         int max1= Math.max(iback,jback);
-        dp[i][j] = Math.max(take,max1);
-        return Math.max(take,max1);
+        dp[i][j] = max1;
+        return max1;
 
     }
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int[][] dp = new int[text1.length()][text2.length()];
-        /*
-        for(int i=0;i<text1.length();i++){
-            for(int j=0;j<text2.length();j++){
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int i=0;i<=text1.length();i++){
+            for(int j=0;j<=text2.length();j++){
                 dp[i][j]=-1;
             }
         }
-        return find(text1.length()-1,text2.length()-1,text1,text2,dp);
-        */
+        return find(text1.length(),text2.length(),text1,text2,dp);
+        
         //tabulation
-        for(int t1 = text2.length()-1;t1>=0;t1--){
-        boolean flag = false;
-        for(int t=t1;t>=0;t--){
-            if(text1.charAt(0)==text2.charAt(t)){
-                dp[0][t1] = 1;
-                flag=true;
-            }
-        }
-        if(!flag){
-            dp[0][t1]=0;
-        }
-        }
-
-        for(int t1 = text1.length()-1;t1>=0;t1--){
-        boolean flag = false;
-        for(int t=t1;t>=0;t--){
-            if(text1.charAt(t)==text2.charAt(0)){
-                dp[t1][0] = 1;
-                flag=true;
-            }
-        }
-        if(!flag){
-            dp[t1][0]=0;
-        }
-        }
-        for(int i=1;i<text1.length();i++){
-            for(int j=1;j<text2.length();j++){
-                int take = 0;
-                if(text1.charAt(i)==text2.charAt(j)){
-                take = dp[i-1][j-1]+1;
-                }
-                int iback=dp[i-1][j];
-                int jback = dp[i][j-1];
-                int max1= Math.max(iback,jback);
-                dp[i][j] = Math.max(take,max1);
-       
-            }
-        }
-        return dp[text1.length()-1][text2.length()-1];
-
+        
 
 
     }
