200eee259c68688deed99d0a2844cbbb59525ca8
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-08-25 08:12:29 +0530

    Time: 109 ms (27.88%), Space: 64.3 MB (12.14%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=2;i<n;i++){
             prime[i]=1;
         }
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(prime[i]==1){
-                for(int j=i*2;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     prime[j]=0;
                 }
             }
