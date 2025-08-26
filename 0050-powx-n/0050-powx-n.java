b922cab5631813e0a27d49586c09c634c99813ec
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-08-26 08:44:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        int nn = Math.abs(n);
+        while(nn>=1){
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }else{
+                x = x*x;
+                nn=nn/2;
+            }
+        }
+        if(n>=0){
+            return ans;
+        }else{
+            return (1.0)/ans;
+        }
+    }
+}
