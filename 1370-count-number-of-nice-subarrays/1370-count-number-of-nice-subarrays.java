71602b2cef1adfc8fa106a1266262b2e13b097c9
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-09-14 23:39:31 +0530

    Time: 14 ms (41.79%), Space: 55 MB (70.2%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,28 +1,27 @@
 class Solution {
-    public int lessthanequalk(int[] nums,int k){
-        if(k<0){
-            return 0;
-        }
+    public static int lessthanequalnice(int[] nums,int k){
         int r=0;
         int l=0;
+        int c=0;
         int ans=0;
-        int codd=0;
         while(r<nums.length){
             if(nums[r]%2!=0){
-                codd++;
+                c++;
             }
-            while(codd>k){
+            while(c>k && l<nums.length){
                 if(nums[l]%2!=0){
-                    codd--;
+                    c--;
                 }
                 l++;
             }
-            ans = ans+r-l+1;
+            ans = ans + r-l+1;
             r++;
         }
         return ans;
     }
     public int numberOfSubarrays(int[] nums, int k) {
-        return lessthanequalk(nums,k)-lessthanequalk(nums,k-1);
+      int g= lessthanequalnice(nums,k);
+      int l= lessthanequalnice(nums,k-1);
+       return g-l;
     }
 }
