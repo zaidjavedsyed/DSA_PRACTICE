2d16d32e627374e31552809acf441da334654a85
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-10-07 13:09:55 +0530

    Time: 4 ms (98.12%), Space: 45 MB (62.13%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,26 +1,45 @@
+/*Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Stack s = new Stack<>();
+int m = Integer.MAX_VALUE;
+Implement the MinStack class:
+void push(int val) pushes the element val onto the stack.
+s.push(val)
+m =Math.min(m,val);
+
+
+*/
 class MinStack {
-Stack<Integer> s = new Stack<>();
-Queue<Integer> q = new PriorityQueue<>();
+   Stack<Integer> mainst;
+   Stack<Integer> minst;
+
     public MinStack() {
-        
+        mainst = new Stack<>();
+        minst = new Stack<>();
     }
     
     public void push(int val) {
-        s.push(val);
-        q.add(val);
+        mainst.push(val);
+        if(minst.isEmpty() ||val <= minst.peek()){
+            minst.push(val);
+        }
     }
     
     public void pop() {
-        q.remove(s.pop());
+        if(mainst.peek().equals(minst.peek())){
+            mainst.pop();
+            minst.pop();
+        }else{
+            mainst.pop();
+        }
+
     }
     
     public int top() {
-    return s.peek();
-    
+        return mainst.peek();
     }
     
     public int getMin() {
-        return q.peek();
+        return minst.peek();
     }
 }
 
