5171b064552ccf84d9b22ecd61eefc1202356164
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2025-10-10 09:01:58 +0530

    Time: 4 ms (34.73%), Space: 44.8 MB (8.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,33 @@
+// Next Greater Element I
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> h1 = new HashMap<>();
+        HashMap<Integer,Integer> hp = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
-            h1.put(nums1[i],i);
+            hp.put(nums1[i],i);
         }
-        int [] arr1 = new int[nums1.length];
-        int r=-1;
-       // ArrayList<Integer> arr = new ArrayList<>();
         Stack<Integer> st = new Stack<>();
-        for(int j=nums2.length-1;j>=0;j--){
+        int[] ans = new int[nums1.length];
+        for(int i=nums2.length-1;i>=0;i--){
+            int r=-1;
             if(st.isEmpty()){
-                 r=-1;
-
+                r=-1;
             }else{
-            while(!st.isEmpty() && nums2[j]>st.peek()){
-                st.pop();
+                while(!st.isEmpty() && nums2[i]>st.peek()){
+                    st.pop();
+                }
+                if(st.isEmpty()){
+                    r=-1;
+                }else{
+                    if(st.peek()>=nums2[i]){
+                        r=st.peek();
+                    }
+                }
             }
-            if(st.isEmpty()){
-                 r=-1;
-
-            }else{
-            if(nums2[j]<st.peek()){
-                 r=st.peek();
-            }}}
-            st.push(nums2[j]);
-            if(h1.get(nums2[j])!=null){
-                int id =h1.get(nums2[j]);
-                arr1[id]=r;
+            st.push(nums2[i]);
+            if(hp.get(nums2[i])!=null){
+                ans[hp.get(nums2[i])]=r; 
             }
-            
-            }
-            return arr1;
-            
+        }
+        return ans;
     }
 }
