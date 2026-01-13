493fb22d84be9615dbd9b8f4ae72b571fcaad9dd
zaidjavedsyed <111593941+zaidjavedsyed@users.noreply.github.com>
2026-01-13 10:33:21 +0530

    Time: 0 ms (100%), Space: 42.2 MB (20.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int find(int i,int dp[]){
-        if(i<=1){
+        if(i==1 || i==0){
             return 1;
         }
         if(dp[i]!=-1){
@@ -8,12 +8,15 @@ class Solution {
         }
         int left = find(i-1,dp);
         int right = find(i-2,dp);
-        dp[i] = left+right;
+        dp[i]=left+right;
         return left+right;
+    
     }
     public int climbStairs(int n) {
-        int[] dp  =new int[n+1];
-        Arrays.fill(dp,-1);
-        return find(n,dp);
+    int dp[] = new int[n+1];
+    for(int i=0;i<=n;i++){
+        dp[i]=-1;
+    }
+    return find(n,dp);
     }
 }
